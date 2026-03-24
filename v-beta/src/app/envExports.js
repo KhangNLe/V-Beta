cb2e0c33a008489800a882bbe0db04bee7c230af
JSirisavath <Jsirisavath123@gmail.com>
2026-03-24 16:56:50 -0500

    fix env exporters names.

--- v-beta/src/app/envExports.js
+++ v-beta/src/app/envExports.js
@@ -1,10 +1,10 @@
-// exports for environment variables defined in .env file
+// exports for environment variables defined in .env.local
 
-// Firebase configuration values
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-export const AUTH_DOMAIN = process.env.NEXT_PUBLIC_AUTH_DOMAIN;
-export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
-export const STORAGE_BUCKET = process.env.NEXT_PUBLIC_STORAGE_BUCKET;
-export const MESSAGING_SENDER_ID = process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID;
-export const APP_ID = process.env.NEXT_PUBLIC_APP_ID;
-export const MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+export const API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+export const AUTH_DOMAIN = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+export const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+export const STORAGE_BUCKET = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+export const MESSAGING_SENDER_ID =
+  process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+export const APP_ID = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+export const MEASUREMENT_ID = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
