bab556f3979466385fd3f6050c28ad49b17ff001
Khang <khang.ng.h.le@gmail.com>
2026-03-27 16:13:43 -0500

    feature: created backend controller to handle request for getting wall sections and problems by wall section id

---
+++ server/src/main/java/edu/ics499/VBeta/controller/WallSectionController.java
@@ -0,0 +1,34 @@
+package edu.ics499.VBeta.controller;
+
+import edu.ics499.VBeta.api.dto.ClimbingProblemResponse;
+import edu.ics499.VBeta.api.dto.WallSectionResponse;
+import edu.ics499.VBeta.application.WallSectionManager;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/home")
+public class WallSectionController {
+    private final WallSectionManager wallSectionManager;
+
+    public WallSectionController(WallSectionManager wallSectionManager){
+        this.wallSectionManager = wallSectionManager;
+    }
+
+    @GetMapping("/wall-sections")
+    @Transactional(readOnly = true)
+    public List<WallSectionResponse> wallSections() {
+        return wallSectionManager.getWallSections();
+    }
+
+    @GetMapping("/wall-sections/{wallSectionId}/problems")
+    @Transactional(readOnly = true)
+    public List<ClimbingProblemResponse> problemsForWallSection(@PathVariable Long wallSectionId) {
+        return wallSectionManager.getClimbingProblemsByWallSectionId(wallSectionId);
+    }
+}   
