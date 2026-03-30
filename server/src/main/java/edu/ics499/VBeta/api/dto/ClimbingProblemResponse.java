b18eb7b8375400a79dc7b9fe913516ea7b5cbf86
Khang <khang.ng.h.le@gmail.com>
2026-03-30 15:54:32 -0500

    feature: fixed /wall-sections/{sectionId}/problems to only send ACTIVE climbing problem instead of all climbing problem from a singular wall

--- server/src/main/java/edu/ics499/VBeta/api/dto/ClimbingProblemResponse.java
+++ server/src/main/java/edu/ics499/VBeta/api/dto/ClimbingProblemResponse.java
@@ -3,7 +3,7 @@ package edu.ics499.VBeta.api.dto;
 public record ClimbingProblemResponse(
         Long problemId,
         String holdColor,
-        String problemInfo,
+        String createdDate,
         String assignedGrade
 ) {
 }
