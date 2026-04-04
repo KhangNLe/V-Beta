26cae8e301ba74f16d6ec8d5b64d53c50815f2c1
Sterling-G <re4816ku@go.minnstate.edu>
2026-04-04 13:07:44 -0500

    added a climbing problem type

---
+++ v-beta/src/types/climbProblem.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef {Object} ClimbProblem
+ * @property {number} problemId
+ * @property {string} holdColor
+ * @property {string} [problemInfo]
+ * @property {string} [createdDate]
+ * @property {string} [assignedGrade]
+ * // Add other properties as needed
+ */
