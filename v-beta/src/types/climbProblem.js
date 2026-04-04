a48cd26376855e9c2bb235c5d27a2ac282f612e7
Sterling-G <re4816ku@go.minnstate.edu>
2026-04-04 14:39:45 -0500

    Changed climbProblem to better match the response from the backend.

--- v-beta/src/types/climbProblem.js
+++ v-beta/src/types/climbProblem.js
@@ -2,8 +2,10 @@
  * @typedef {Object} ClimbProblem
  * @property {number} problemId
  * @property {string} holdColor
- * @property {string} [problemInfo]
+ * @property {string} [info]
  * @property {string} [createdDate]
  * @property {string} [assignedGrade]
+ * @property {string} [perceiveGrade]
+ * @property {Array<unknown>} [discussion]
  * // Add other properties as needed
  */
