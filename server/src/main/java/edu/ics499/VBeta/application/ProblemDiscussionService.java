4985edbd2b57a859eb8e848b6893f4ae71fd21df
Khang <khang.ng.h.le@gmail.com>
2026-04-06 11:10:27 -0500

    set up controller and service for user comment

---
+++ server/src/main/java/edu/ics499/VBeta/application/ProblemDiscussionService.java
@@ -0,0 +1,52 @@
+package edu.ics499.VBeta.application;
+
+import edu.ics499.VBeta.api.dto.DiscussionCommentRequest;
+import edu.ics499.VBeta.application.support.ClimbingProblemDiscussionManager;
+import edu.ics499.VBeta.application.support.ClimbingProblemManager;
+import edu.ics499.VBeta.domain.model.ClimbingProblem;
+import edu.ics499.VBeta.domain.model.UserAccount;
+import edu.ics499.VBeta.repository.DiscussionCommentRepository;
+import edu.ics499.VBeta.application.support.UserAccountManager;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@Transactional
+public class ProblemDiscussionService {
+    private final UserAccountManager userAccountManager;
+    private final ClimbingProblemManager climbingProblemManager;
+    private final ClimbingProblemDiscussionManager climbingProblemDiscussionManager;
+
+    public ProblemDiscussionService(UserAccountManager userAccountManager,
+                                    ClimbingProblemManager climbingProblemManager,
+                                    ClimbingProblemDiscussionManager climbingProblemDiscussionManager){
+        this.userAccountManager = userAccountManager;
+        this.climbingProblemManager = climbingProblemManager;
+        this.climbingProblemDiscussionManager = climbingProblemDiscussionManager;
+    }
+
+    public void addComment(String firebaseUid, DiscussionCommentRequest request){
+        UserAccount account = getUserAccount(firebaseUid);
+        ClimbingProblem problem = getClimbingProblem(request.problemId());
+    }
+
+    private UserAccount getUserAccount(String firebaseUid){
+        UserAccount account =  userAccountManager.findUserAccount(firebaseUid);
+        if (account == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    "User Account with the unique firebase ID does not exist. Please log in and try again.");
+        }
+        return account;
+    }
+
+    private ClimbingProblem getClimbingProblem(Long problemId){
+        ClimbingProblem problem = climbingProblemManager.getActiveProblem(problemId);
+        if (problem == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("The problem with ID %d does not exist or no longer active.", problemId));
+        }
+        return problem;
+    }
+}
--- server/src/main/java/edu/ics499/VBeta/controller/ProblemDiscussionController.java
+++ server/src/main/java/edu/ics499/VBeta/controller/ProblemDiscussionController.java
@@ -1,26 +1,21 @@
 package edu.ics499.VBeta.controller;
 
 import edu.ics499.VBeta.api.dto.DiscussionCommentRequest;
-import edu.ics499.VBeta.api.dto.UserCommentData;
-import edu.ics499.VBeta.application.CommentManager;
-import edu.ics499.VBeta.domain.model.LifecycleStatus;
+import edu.ics499.VBeta.application.ProblemDiscussionService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.util.*;
 
 @RestController
 @RequestMapping("/discussion")
 public class ProblemDiscussionController {
-    private final CommentManager commentManager;
+    private final ProblemDiscussionService problemDiscussionService;
 
-    public ProblemDiscussionController(CommentManager commentManager){
-        this.commentManager = commentManager;
+    public ProblemDiscussionController(ProblemDiscussionService problemDiscussionService){
+        this.problemDiscussionService = problemDiscussionService;
     }
 
     @PostMapping("/add-comments")
@@ -31,7 +26,7 @@ public class ProblemDiscussionController {
                     "Missing or invalid authentication");
         }
         try {
-            commentManager.addComment(String.valueOf(auth.getPrincipal()), request);
+            problemDiscussionService.addComment(String.valueOf(auth.getPrincipal()), request);
 
         } catch (IllegalStateException e){
             throw new ResponseStatusException(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS, e.getMessage());
