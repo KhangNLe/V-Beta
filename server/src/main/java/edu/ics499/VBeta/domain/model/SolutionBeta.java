23b83c30edc8a34f975630fd0f5cc64493c17c37
Khang <khang.ng.h.le@gmail.com>
2026-04-07 16:42:34 -0500

    feature: add public url into climbing problem discussion session

---
+++ server/src/main/java/edu/ics499/VBeta/domain/model/SolutionBeta.java
@@ -0,0 +1,36 @@
+package edu.ics499.VBeta.domain.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Solution_Beta")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SolutionBeta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "beta_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_beta_id", referencedColumnName = "user_beta_id")
+    private UserBeta userBeta;
+
+    @Column(name = "beta_name", nullable = false, length = 125)
+    private String betaName;
+
+    @Column(name = "video_url", length = 150)
+    private String videoURL;
+
+    @Column(name = "create_date")
+    private LocalDateTime createDate;
+}
