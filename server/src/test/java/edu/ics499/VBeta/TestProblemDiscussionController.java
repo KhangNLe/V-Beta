3f3ceebf39ac9893d0943e002bb4c61ba386f7d1
Khang <khang.ng.h.le@gmail.com>
2026-04-08 23:02:48 -0500

    fix: change deprecated class in unit test

--- server/src/test/java/edu/ics499/VBeta/TestProblemDiscussionController.java
+++ server/src/test/java/edu/ics499/VBeta/TestProblemDiscussionController.java
@@ -9,10 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -34,10 +34,10 @@ public class TestProblemDiscussionController {
     @Autowired
     ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private ProblemDiscussionService problemDiscussionService;
 
-    @MockBean
+    @MockitoBean
     private AuthorizationService authorizationService;
 
     @Test
