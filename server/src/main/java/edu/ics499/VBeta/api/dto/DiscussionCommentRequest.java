6244926d0f0fa6eea45724799de72d07880416c9
Khang <khang.ng.h.le@gmail.com>
2026-04-16 12:33:17 -0500

    change jakarta requirement for DiscussionCommentRequest

--- server/src/main/java/edu/ics499/VBeta/api/dto/DiscussionCommentRequest.java
+++ server/src/main/java/edu/ics499/VBeta/api/dto/DiscussionCommentRequest.java
@@ -1,12 +1,13 @@
 package edu.ics499.VBeta.api.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record DiscussionCommentRequest(
         @NotNull
         Long problemId,
-        @NotNull @Size(max = 250)
+        @NotBlank @Size(max = 250)
         String commentInfo
 ) {
 }
