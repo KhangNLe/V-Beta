3dfa598fa7494eceefc0a21f9a2b783d0bf6abb0
Khang <khang.ng.h.le@gmail.com>
2026-05-04 10:01:50 -0500

    feat: add domain entity and repository for the discussion_root table

---
+++ server/src/main/java/edu/ics499/VBeta/domain/model/DiscussionRoot.java
@@ -0,0 +1,58 @@
+package edu.ics499.VBeta.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Discussion_Root")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiscussionRoot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "discussion_id")
+    private Long discussionId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_discussion_id", referencedColumnName = "discussion_id")
+    private DiscussionRoot parent;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "problem_id", referencedColumnName = "problem_id")
+    private ClimbingProblem problem;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private UserAccount userAccount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "discussion_type", nullable = false)
+    private DiscussionType discussionType;
+
+    @Column(name = "create_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "deleted_by", referencedColumnName = "user_id")
+    private UserAccount deletedBy;
+
+    @Column(name = "deleted_reason", length = 100)
+    private String  deletedReason;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (createdAt == null) {
+            createdAt = LocalDateTime.now();
+        }
+    }
+}
---
+++ server/src/main/java/edu/ics499/VBeta/repository/DiscussionRootRepository.java
@@ -0,0 +1,16 @@
+package edu.ics499.VBeta.repository;
+
+import edu.ics499.VBeta.domain.model.DiscussionRoot;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface DiscussionRootRepository extends JpaRepository<DiscussionRoot, Long> {
+    List<DiscussionRoot> findByParent_DiscussionId(Long parentDiscussionId);
+    List<DiscussionRoot> findByParent_DiscussionIdOrderByCreatedAtDesc(Long parentDiscussionId);
+
+    List<DiscussionRoot> findByProblem_ProblemIdAndParentIsNullOrderByCreatedAtDesc(Long problemId);
+    List<DiscussionRoot> findByParent_DiscussionIdAndDeletedAtIsNullOrderByCreatedAtAsc(Long parentDiscussionId);
+
+    List<DiscussionRoot> findByProblem_ProblemIdAndParentIsNullAndDeletedAtIsNullOrderByCreatedAtDesc(Long problemId);
+}
